e26f94b6df02dc05b6b77ac3a74655219d765eea
Jon <jon.bloomer@iag.com.au>
2018-03-24 19:12:49 +1100

    setup of test react app with markdown

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown'
+import logo from './logo.svg';
+import './App.css';
+import myMarkdownFile from './intro.md';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdownContent: null
+    }
+    this.getMarkdown()
+  }
+  getMarkdown() {
+    fetch(myMarkdownFile)
+    .then(response => response.text())
+    .then(text => {
+      // Logs a string of Markdown content.
+      // Now you could use e.g. <rexxars/react-markdown> to render it.
+      console.log(text);
+      this.setState({markdownContent: text})
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="App-intro">
+        {this.state.markdownContent !== null &&
+          <ReactMarkdown source={this.state.markdownContent} />
+        }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/intro.md
@@ -0,0 +1,7 @@
+This is a markdown example
+
+# trying a heading
+
+How about a [link](https://google.com)
+
+## Markdown FTW!
