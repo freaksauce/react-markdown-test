419dcb8e3cd452d60a22a8c140aff3b00e180cc6
Jon <jon.bloomer@iag.com.au>
2018-03-25 13:51:44 +1100

    updated app to load 3 markdown files, 2 from github

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      markdownContent: null
+      m1: null,
+      m2: null,
+      m3: null
     }
-    this.getMarkdown()
+    this.getMarkdown(myMarkdownFile, 'm1')
+    this.getMarkdown('https://raw.githubusercontent.com/freaksauce/react-markdown-test/master/src/content/foo/foo.md', 'm2')
+    this.getMarkdown('https://raw.githubusercontent.com/freaksauce/react-markdown-test/master/src/content/bar/bar.md', 'm3')
   }
-  getMarkdown() {
-    fetch(myMarkdownFile)
+  getMarkdown(file, stateVar) {
+    fetch(file, {mode: 'cors'})
     .then(response => response.text())
     .then(text => {
-      this.setState({markdownContent: text})
+      this.setState({[stateVar]: text})
     });
   }
   render() {
@@ -27,8 +31,16 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="App-intro">
-        {this.state.markdownContent !== null &&
-          <ReactMarkdown source={this.state.markdownContent} />
+        {this.state.m1 !== null &&
+          <ReactMarkdown source={this.state.m1} />
+        }
+        <hr />
+        {this.state.m2 !== null &&
+          <ReactMarkdown source={this.state.m2} />
+        }
+        <hr />
+        {this.state.m3 !== null &&
+          <ReactMarkdown source={this.state.m3} />
         }
         </div>
       </div>
